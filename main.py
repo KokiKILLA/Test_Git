57a68fd95940b94b3151ef47723ee177e9ed485a
Student1 <test@ya.ru>
2025-06-27 17:03:53 +0300

    add very strange things

--- main.py
+++ main.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
   return 'Hack the planet'
 
+app.route('/index')
+def pascal():
+  return "very strange things"
+
 if __name__ == '__main__':
   app.run(debug=True)
