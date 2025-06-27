eb373d67ec6f36dc5adb408bf1ffbce09c84d960
Student1 <test@ya.ru>
2025-06-27 17:02:02 +0300

    add parity check

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
@@ -6,9 +7,13 @@ app = Flask(__name__)
 def hello_world():
   return 'Hack the planet'
 
-@app.route('/index')
-def death_code():
-  return 'this is pascal'
+@app.route('/parity_check')
+def parity_check():
+  random_number = random.randrange(0, 9)
+  if random_number % 2 == 0:
+    return "чет"
+  else:
+    return "нечет"
 
 if __name__ == '__main__':
   app.run(debug=True)
