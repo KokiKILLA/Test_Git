8051a7510dc112742ddbcc98ffd3f7938fb95d95
Student1 <test@ya.ru>
2025-06-27 16:55:40 +0300

    parity check

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
   return 'Hack the planet'
+
+@app.route('/index')
+def death_code():
+  return 'this is pascal'
 
 if __name__ == '__main__':
   app.run(debug=True)
