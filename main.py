b56a12c569b300e57b3cacb2e5f72d93d3b44a64
Student1 <test@ya.ru>
2025-06-27 16:52:10 +0300

    add /index

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-  return 'Hack the planet'
-
-if __name__ == '__main__':
-  app.run(debug=True)
+def index():
+  return "это паскаль"
